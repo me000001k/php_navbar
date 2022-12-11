ae25f339e9de30ed415466a894244ae521313b54
me000001k <117127957+me000001k@users.noreply.github.com>
2022-12-11 15:51:46 +0300

    Commenting page access example

--- src/scripts/get_page.php
+++ src/scripts/get_page.php
@@ -1,23 +1,25 @@
 <?php
 
-if ($_GET['cat_id'] === '1'){
-    
-    header('Location: http://localhost/navbar_php/main.php');
-    exit();
-    
-} elseif ($_GET['cat_id'] === '2') {
-    
-    header('Location: http://localhost/navbar_php/profile.php');
-    exit();
-    
-} elseif ($_GET['cat_id'] === '3'){
-    
-    header('Location: http://localhost/navbar_php/info.php');
-    exit();
-    
-} else {
-    
-    echo 'Error';
-    
-}
+// For example
+
+//if ($_GET['cat_id'] === '1'){
+//    
+//    header('Location: http://localhost/navbar_php/main.php');
+//    exit();
+//    
+//} elseif ($_GET['cat_id'] === '2') {
+//    
+//    header('Location: http://localhost/navbar_php/profile.php');
+//    exit();
+//    
+//} elseif ($_GET['cat_id'] === '3'){
+//    
+//    header('Location: http://localhost/navbar_php/info.php');
+//    exit();
+//    
+//} else {
+//    
+//    echo 'Error';
+//    
+//}
 
